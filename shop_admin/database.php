80d49f878ea19e795b851beb1ddd4c87958334a5
taulatai123 <taino846@gmail.com>
2019-06-21 18:49:59 +0700

    update

--- shop_admin/database.php
+++ shop_admin/database.php
@@ -1,21 +1,18 @@
 <?php
 	function getDB()
 	{
-		if (empty(getenv("DATABASE_URL"))){
-			echo '<p>The DB does not exist</p>';
-			$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=asm', 'postgres', '123456');
-		}  else {
-			 echo '<p>The DB exists</p>';
-			 echo getenv("dbname");
-		   $db = parse_url(getenv("DATABASE_URL"));
-		   $pdo = new PDO("pgsql:" . sprintf(
-				"ec2-54-221-243-211.compute-1.amazonaws.com;port=5432;user=docokbxiffiwzw;password=5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8",
-				$db["host"],
-				$db["port"],
-				$db["user"],
-				$db["pass"],
-				ltrim($db["path"], "/")
-		   ));
+		$dsn = "pgsql:host=localhost;port=5432;dbname=mydb";
+		$username = 'postgres';
+		$password = '123456';
+		try{
+			$db = new PDO($dsn, $username, $password);
+			$dsn = "pgsql:host=ec2-54-221-243-211.compute-1.amazonaws.com;port=5432;dbname=d6jmv4urb51qs8";
+			$username = 'docokbxiffiwzw';
+			$password = '5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8';
+			return $db;
+		} catch (PDOException $e) {
+			$error_message = $e->getMessage();
+			echo "Eroor connecting to database".$error_message;
 		}
 	}
 ?>
