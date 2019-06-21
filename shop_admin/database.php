4c9d6a03d9a6ffaa8eab0333e06f883a93ee160c
taulatai123 <taino846@gmail.com>
2019-06-21 18:28:11 +0700

    update

--- shop_admin/database.php
+++ shop_admin/database.php
@@ -1,7 +1,7 @@
 <?php
 	function getDB()
 	{
-		$dsn = "pgsql:host=localhost;dbname=asm";
+		$dsn = "pgsql:host=localhost;port=5432;dbname=asm";
 		$username = "docokbxiffiwzw";
 		$password = '5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8';
 		$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
