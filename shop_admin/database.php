5558eb05ee704c51cacf02f208a7f886cdd66d12
taulatai123 <taino846@gmail.com>
2019-06-21 22:49:48 +0700

    taa

--- shop_admin/database.php
+++ shop_admin/database.php
@@ -1,14 +1,13 @@
 <?php
 	function getDB()
 	{
-		$dsn = "mysql:host=localhost;dbname=tai_shop";
-		$username = "root";
-		$password = '';
-		$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+		$dsn = "pgsql:host=ec2-54-221-243-211.compute-1.amazonaws.com;port=5432;dbname=asm";
+		$username = "rootdocokbxiffiwzw";
+		$password = '5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8';
+
 
 		try{
-			$db = new PDO($dsn, $username, $password, $options);
+			$db = new PDO($dsn, $username, $password);
 			return $db;
 		} catch (PDOException $e) {
 			$error_message = $e->getMessage();
