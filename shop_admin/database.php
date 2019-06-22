34b4bd780e3fbd63b0ed5c93797c96b85a89979e
taulatai123 <taino846@gmail.com>
2019-06-22 17:22:12 +0700

    aa

--- shop_admin/database.php
+++ shop_admin/database.php
@@ -1,6 +1,6 @@
 <?php
 	function getDB()
 	{
-		$dbcon = pg_connect("host=ec2-54-221-243-211.compute-1.amazonaws.com port=5432 dbname=d6jmv4urb51qs8 user=docokbxiffiwzw password=5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8");
+		$dbcon = pg_connect("host='ec2-54-221-243-211.compute-1.amazonaws.com' port='5432' dbname='d6jmv4urb51qs8' user='docokbxiffiwzw' password='5a923307b82e04e852bb40397b2335a069f3ec647faaebd08ce96877b001baf8'");
 	}
 ?>
