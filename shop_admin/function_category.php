06f89166893618b527c697fb50506b67f89a38af
taulatai123 <taino846@gmail.com>
2019-06-22 17:56:41 +0700

    tai

--- shop_admin/function_category.php
+++ shop_admin/function_category.php
@@ -17,12 +17,12 @@
 	function add_category($categoryname, $description, $by_user){
 		$db = getDB();// Connect to database
 		$query ="INSERT INTO categoties (categoryname, description, by_user)
-				VALUES (?,?,?)";
+				VALUES (:categoryname,:description,:by_user)";
 		try {
 			$statement = $db->prepare($query);
-			$statement->bindParam(1,$categoryname);
-			$statement->bindParam(2,$description);
-			$statement->bindParam(3,$by_user);
+			$statement->bindParam(':categoryname',$categoryname);
+			$statement->bindParam(':description',$description);
+			$statement->bindParam(':by_user',$by_user);
 			$statement->execute();
 			$statement->closeCursor();			
 		} catch (PDOException $e) {
@@ -34,11 +34,11 @@
 	function get_category_by_id($categoryid){
 		$db = getDB();// Connect to database
 		$query ="SELECT * FROM categoties 
-				WHERE categoryid=? 
+				WHERE categoryid=:categoryid 
 				ORDER BY categoryid";
 		try {
 			$statement = $db->prepare($query);
-			$statement->bindParam(1,$categoryid);
+			$statement->bindParam(':categoryid',$categoryid);
 			$statement->execute();
 			$result = $statement->fetch();
 			$statement->closeCursor();
@@ -52,14 +52,16 @@
 	function delete_category_by_id($categoryid){
 		$db = getDB();// Connect to database
 		$query ="DELETE  FROM categoties 
-				WHERE categoryid=?";
+				WHERE categoryid=:categoryid";
 		try {
-			$db->exec($query);
+			$statement = $db->prepare($query);
+			$statement->bindParam(':categoryid',$categoryid);
+			$statement->execute();			
+			$statement->closeCursor();		
 		} catch (PDOException $e) {
 			$error_message = $e->getMessage();
 			echo "Error execute query statement:".$error_message; 
 		}
-		$db = null;
 	}
 
 	function update_category($categoryid,$categoryname,$description,$by_user){
